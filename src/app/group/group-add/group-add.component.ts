560c5db99d274be09ee2f6751e05ed1d0357fcd5
braxyxx <braxyxx31@gmail.com>
2018-05-29 18:24:38 +0200

    Add-group component

---
+++ src/app/group/group-add/group-add.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Group } from '../shared/group';
+import { GroupService } from '../shared/group.service';
+import { ToastrService } from 'ngx-toastr';
+import { isNullOrUndefined } from 'util';
+
+@Component({
+  selector: 'app-group-add',
+  templateUrl: './group-add.component.html',
+  styleUrls: ['./group-add.component.css']
+})
+export class GroupAddComponent implements OnInit {
+
+  group : Group;
+  
+  constructor(private groupService: GroupService, private toastrService : ToastrService) { }
+  
+  ngOnInit() {
+    this.group = {
+      name: "",
+      nextNotificationDate: null
+    };
+  }
+  
+  sendNewGroup() {
+    this.groupService.postGroup(this.group).subscribe(data => {
+      this.toastrService.success('Envoyée', 'Votre groupe a bien été envoyé.');
+      this.group = {
+        name: "",
+        nextNotificationDate: null
+      };
+    });
+  }
+
+}
