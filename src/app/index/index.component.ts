3a68d03279981b751c085b3b1b2debc2e4713e71
MissYoruwara <cecel31@free.fr>
2018-06-08 15:00:43 +0200

    session pour la connexion

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -10,6 +10,7 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    sessionStorage.removeItem('user');
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { LoginService } from './shared/login.service';
 import * as shajs from 'sha.js';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,13 @@ import * as shajs from 'sha.js';
 export class LoginComponent implements OnInit {
   
   private user;
+  private connectedUser;
+
   
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private loginService: LoginService, private router: Router, private toastrService : ToastrService) {}
   
   ngOnInit() {
+    sessionStorage.removeItem('user');
     this.user = {
       email: "",
       password: ""
@@ -26,7 +30,14 @@ export class LoginComponent implements OnInit {
     console.log(this.user.password);
     this.user.password = shajs('sha256').update(this.user.password).digest('hex');
     this.loginService.findExistingUser(this.user).subscribe(data => {
-      this.router.navigate(["/index"]);
+      if (data) {
+        this.connectedUser.id = data[0].id;
+        this.connectedUser.type = data[0].type
+        sessionStorage.setItem('user', JSON.stringify(this.connectedUser));
+        this.router.navigate(["/index"]);
+      } else {
+        this.toastrService.error('Mauvais identifiants !', 'Erreur');
+      }
     });
   }
 }
