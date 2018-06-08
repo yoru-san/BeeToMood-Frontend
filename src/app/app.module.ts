d668ac210cec6a8462c487eeb12625023a731fb9
braxyxx <braxyxx31@gmail.com>
2018-06-08 16:19:19 +0200

    app module et group-add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { GroupListComponent } from './group/group-list/group-list.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { ParameterComponent } from './parameter/parameter.component';
+import { ParameterService } from './parameter/shared/parameter.service';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/shared/login.service';
 import { GroupAddComponent } from './group/group-add/group-add.component';
@@ -56,7 +57,7 @@ import { IndexComponent } from './index/index.component';
     NgbModule.forRoot(),
     DlDateTimePickerDateModule
   ],
-  providers: [UserService, ReviewService, GroupService, LoginService],
+  providers: [UserService, ReviewService, GroupService, LoginService, ParameterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
